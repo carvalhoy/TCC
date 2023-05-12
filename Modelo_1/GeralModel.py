cb4067e2c2cf90df6f823fa5be523585c19646fa
Matheus <matheusbelchior23@gmail.com>
2023-05-11 21:15:49 -0300

    Alteração modelo geral

--- Modelo_1/GeralModel.py
+++ Modelo_1/GeralModel.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime as dt
 from lmfit import minimize, Parameters, Parameter, report_fit
+from simple_chalk import chalk
 
 
 
@@ -43,20 +44,20 @@ def ajustarXlsx(caminho, parametrosDadosXlsx):
     [tempoInicial, tempoFinal, totalPontos, digitosDecimais] = parametrosDadosXlsx
     dados = pd.read_excel(caminho, header=None, names=['tempo', 'concentração'], decimal=',')
     dados['tempo'] = np.linspace(tempoInicial, tempoFinal, totalPontos)
-    dados['concentração'] = round(dados['concentração'], digitosDecimais)
+    dados['concentração'] = round(dados['concentração']/1000, digitosDecimais)
     return dados
 
 # Integração usando paras e converte o resultado de matriz numpy em transforma em dataframe para obter a coluna de índice 2 - produtos:
-def residual(paras, t_step, data, max_step, t_solve_ivp, x, indice, metodoIntegracao):
-    model = integracao(metodoIntegracao, t_step, paras, max_step, t_solve_ivp, x)
+def residual(paras, t_step, data, t_solve_ivp, x, rtol, atol, indice, metodoIntegracao):
+    model = integracao(metodoIntegracao, t_step, paras, t_solve_ivp, x, rtol, atol)
     model = pd.DataFrame(model.y).transpose()
     modelVariaveldoModelo = model.iloc[:, indice]
     error = (modelVariaveldoModelo - data).ravel()
     ##print(error)
     return error
     
-def integracao(metodoIntegracao, t_step, params, max_step, t_solve_ivp, x):
-    solve_ivp = scipy.integrate.solve_ivp(model, t_step, x, metodoIntegracao, t_eval=t_solve_ivp, args=[params], max_step=max_step)
+def integracao(metodoIntegracao, t_step, params, t_solve_ivp, x, rtol, atol):
+    solve_ivp = scipy.integrate.solve_ivp(model, t_step, x, metodoIntegracao, t_eval=t_solve_ivp, args=[params], rtol=rtol, atol=atol)
     return solve_ivp
     
 def plotagem(solve_ivp, dados_P, dados_S):
@@ -74,39 +75,49 @@ def main():
     parametrosDadosXlsx = [0, 240, 25, 3] #tempo inicial, tempo final, número de pontos, algarismos significativos
     dados_P = ajustarXlsx("../xlsx1/produto.xlsx", parametrosDadosXlsx)
     dados_S = ajustarXlsx("../xlsx1/substrato.xlsx", parametrosDadosXlsx)
+    #print(f'Substrato: \n ${dados_S}')  
+    #print(f'Produto: \n ${dados_P}')  
 
-    #condições iniciais das variáveis dos balanços, intervalo de tempo considerado, 
-    x = [42500.0, 25200.0, 0.0]
+     #condições iniciais das variáveis dos balanços: substrato, biomassa, produto;
+    x = [42.5, 25.2, 0.0]
+     #intervalo de integração, max_step e dados no tempo a serem retornados pela função integrate.solve_ivp:
     t_step = [0, 240]
     t_solve_ivp = dados_P['tempo']
-    #intervalo de integração, max_step e dados no tempo a serem retornados pela função integrate.solve_ivp:
-    metodoIntegracao = 'RK45'
-    max_step = 0.05
+    metodosIntegracao = ['RK45', 'RK23', 'DOP853', 'Radau', 'BDF', 'LSODA']
+    metodoIntegracao = metodosIntegracao[0]
+    rtol = 3e-14
+    atol = 1e-18
     
-    ##definindo os parâmetros declarados como argumento para o solve_ivp
-    ##params = (1.46484375e-03, 6.11696777e+00, 2.82084961e+02,   5.57128906e-01, 4.22119141e-01, 2.41699219e-03, 0.)
-
-    ## Definindo parâmetros para Curve Fitting:
+     ## Definindo parâmetros para Curve Fitting:
     paras = Parameters()
     paras.add('S_in', value=0., vary=False)
-    paras.add('mumax_X', value=0.3, min=0.0001)
-    paras.add('K_S', value=0.10, min=0.0001)
-    paras.add('Y_X_S', value=0.7, min=0)
-    paras.add('Y_P_S', value=0.3, min=0)
-    paras.add('k_dec', value=0.00015, min=0)
+    paras.add('mumax_X', value=0.002, min=0.0001, max=0.015)
+    paras.add('K_S', value=3.5, min=0.1, max=4)
+    paras.add('Y_X_S', value=0.1, min=0, max=1)
+    paras.add('Y_P_S', value=0.7, min=0.1, max=1.5)
+    paras.add('k_dec', value=0.01, min=0.001, max=1)
     paras.add('D', value=0., vary=False)
     
-    metodoMinimizacao = 'leastsq'
+    metodoMinimizacao = 'Nelder-Mead'
     
-    ## Otimização para Substrato (0), Biomassa (1), Produto (2):
+     ## Otimização para Substrato (0), Biomassa (1), Produto (2):
     indice = [0, 1, 2]
     dadoOtimizacao = [dados_S['concentração'], dados_P['concentração']]
     
-    resultProduto = minimize(residual, paras, args=(t_step, dadoOtimizacao[1], max_step, t_solve_ivp, x, indice[2], metodoIntegracao), method=metodoMinimizacao)  # leastsq
-    report_fit(resultProduto) 
+    # print("minimize chamada para produto")
+    # resultProduto = minimize(residual, paras, args=(t_step, dadoOtimizacao[1], t_solve_ivp, x, rtol, atol, indice[2], metodoIntegracao), method=metodoMinimizacao)  # leastsq
+    # #report_fit(resultProduto) 
+    # #execução da função de integração e plotagem do gráfico: 
+    # print("plotagem chamada para produto")
+    # plotagem(integracao(metodoIntegracao, t_step, resultProduto.params, t_solve_ivp, x, rtol, atol), dados_P, dados_S)
     
-    
+    print(chalk.red("minimize chamada para substrato"))
+    resultSubstrato = minimize(residual, paras, args=(t_step, dadoOtimizacao[0], t_solve_ivp, x, rtol, atol, indice[0], metodoIntegracao), method=metodoMinimizacao)  # leastsq
+    #report_fit(resultProduto) 
+    report_fit(resultSubstrato)
     #execução da função de integração e plotagem do gráfico: 
-    ##plotagem(integracao(metodoIntegracao, t_step, params, max_step, t_solve_ivp, x), dados_P, dados_S)
+    print(chalk.green("plotagem chamada para Substrato"))
+    plotagem(integracao(metodoIntegracao, t_step, resultSubstrato.params, t_solve_ivp, x, rtol, atol), dados_P, dados_S)
+    
     
 main()
